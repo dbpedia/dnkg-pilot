b4fca20ef63c8f203fa058d7b5197a5f06d3e105
Marvin Hofer <vehnem@yahoo.de>
2020-09-14 12:38:54 +0200

    Update generate.scala: find deepProp

--- scripts/generate.scala
+++ scripts/generate.scala
@@ -1,76 +1,122 @@
-import java.net.URI
 import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths, StandardOpenOption}
 
-import $ivy.`org.apache.commons:commons-compress:1.20`
-import org.apache.commons.compress.compressors.bzip2._
-
-import $ivy.`com.lihaoyi::ammonite-ops:2.2.0`
 import $ivy.`com.google.guava:guava:18.0`
-import $ivy.`org.apache.jena:jena-core:3.16.0`
+import $ivy.`com.lihaoyi::ammonite-ops:2.2.0`
+import $ivy.`org.apache.commons:commons-compress:1.20`
 import $ivy.`org.apache.jena:jena-arq:3.16.0`
+import $ivy.`org.apache.jena:jena-core:3.16.0`
 import ammonite.ops._
-import java.io.File
-import java.net.URL
-import java.net.URLEncoder
-import org.apache.jena.query.Query
-import org.apache.jena.query.QueryFactory
-import org.apache.jena.sparql.algebra.Algebra 
-import org.apache.jena.sparql.algebra.Op
+import org.apache.jena.graph.Node
+import org.apache.jena.query.{Query, QueryFactory}
+import org.apache.jena.sparql.algebra.op.{OpBGP, OpPath, OpTriple}
+import org.apache.jena.sparql.algebra.{Algebra, Op, OpVisitorBase, OpWalker}
+import org.apache.jena.sparql.path.{P_Link, P_Seq, PathVisitorBase}
 
-
-
+import scala.collection.mutable.ListBuffer
+import scala.jdk.CollectionConverters.ListHasAsScala
 
 /**
-  * generate usefull csv per cartridge 
-  *
-  * amm ./generate.sc split --dir ""
-  *
-  * @param dir directory to deploy, e.g. '/var/www'
-  */
-//todo add dir  
+ * generate usefull csv per cartridge
+ *
+ * amm generate.scala --stats ../stats --cartridges ../cartridges
+ *
+ * @param stats directory to deploy, e.g. '/var/www'
+ */
+//todo add dir
 @main
-def generate(): Unit = {
-	val dir = pwd / "stats"
+def generate(stats: os.Path, cartridges: os.Path): Unit = {
 
-  val cartridges = ls! (pwd/"cartridges")
-  for (c <- cartridges){
-	  val cartname = (c relativeTo pwd).last
-      val outFile:os.Path = dir/(cartname+".tsv")
-	  rm! outFile
+  val cartridgesIter = ls ! cartridges
+  for (c <- cartridgesIter) {
+    val cartName = (c relativeTo pwd).last
+    val outFile: os.Path = stats / (cartName + ".tsv")
+    rm ! outFile
 
-	  println("############")
-	  println(cartname)
-	  println("############")
+    println("############")
+    println(cartName)
+    println("############")
 
-      //TODO add another loop for class
-      val classname = "todoclass"
-	  for (f <- (ls.rec! c).filter(_.toString.endsWith("pt-construct")) ){
-		val pt = f.last.replace(".pt-construct","")  
-		println(pt)
-		try{
-			
-			
-		//todo add prefixes to queries
-		//todo "visit the algebra Ops and pick the most bottom prop "
-		// see visitors https://jena.apache.org/documentation/query/manipulating_sparql_using_arq.html
+    //TODO add another loop for class
+    val classname = "todoclass"
+    for (f <- (ls.rec ! c).filter(_.toString.endsWith("pt-construct"))) {
+      val pt = f.last.replace(".pt-construct", "")
+      println("dbo:"+pt)
+      try {
 
-		val q = read! f
-		val query:Query = QueryFactory.create(q) ;
-        val op:Op = Algebra.compile(query) ;
-        
-        println(q)
-        println(query)
-        println(op)
-} catch {case e:Exception => println("fix "+f);println(e)}
-        // todo use algebra to get "deepest" prop
-        val deepestProp = "http://todo.org/"
-        
-       //TODO classname
-		write.append(outFile,(cartname+"\t"+classname+"\t"+pt+"\t"+deepestProp+"\n").getBytes(StandardCharsets.UTF_8))
-	  }
-   }
-   
+        //todo add prefixes to queries
+        //todo DONE(await check) "visit the algebra Ops and pick the most bottom prop"
+        // see visitors https://jena.apache.org/documentation/query/manipulating_sparql_using_arq.html
+
+        val q = read ! f
+        val query: Query = QueryFactory.create(q);
+        val op: Op = Algebra.compile(query);
+
+        val collector = new DeepPropVisitor()
+
+        OpWalker.walk(op, collector);
+
+        val deepestProp = collector.getProps().last
+        println("+- DeepProp:\n\t"+deepestProp)
+
+        println("+- QUERY:\n\t" + query.toString().split("\n").mkString("\n\t"))
+        println("+- OP:\n\t" + op.toString().split("\n").mkString("\n\t"))
+
+        //TODO classname
+        write.append(outFile, (cartName + "\t" + classname + "\t" + pt + "\t" + deepestProp + "\n").getBytes(StandardCharsets.UTF_8))
+      } catch {
+        case e: Exception => println("fix " + f); e.printStackTrace()
+      }
+    }
+  }
+}
+
+// TODO recursive walk execution
+class DeepPropVisitor() extends OpVisitorBase {
+
+  private val props = new ListBuffer[Node]()
+
+  def getProps(): ListBuffer[Node] = {
+    props
+  }
+
+
+  override def visit(opTriple: OpTriple): Unit = {
+    props.append(opTriple.getTriple.getPredicate)
+  }
+
+  override def visit(opBGP: OpBGP): Unit = {
+    opBGP.getPattern.getList.asScala.foreach(triple => {
+      props.append(triple.getPredicate)
+    })
+  }
+
+  override def visit(opPath: OpPath): Unit = {
+    try {
+
+      // TODO check obj binding
+      val epv = new LastPathSegmentVisitor
+      opPath.getTriplePath.getPath.visit(epv)
+      props.append(epv.getLast)
+    } catch {
+      case e: Exception => println("###"); e.printStackTrace()
+    }
+  }
 
 }
 
+class LastPathSegmentVisitor() extends PathVisitorBase {
+
+  private var last: Node = null
+
+  def getLast: Node = {
+    last
+  }
+
+  override def visit(pathNode: P_Link): Unit = {
+    last = pathNode.getNode
+  }
+
+  override def visit(pathSeq: P_Seq): Unit = {
+    pathSeq.getRight.visit(this)
+  }
+}
