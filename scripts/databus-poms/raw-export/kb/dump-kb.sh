5d951c169252c3116e18826229de35f831cdb029
root <root@dbpedia-mappings.tib.eu>
2020-10-01 12:59:14 +0200

    dump links from kb

--- scripts/databus-poms/raw-export/kb/dump-kb.sh
+++ scripts/databus-poms/raw-export/kb/dump-kb.sh
@@ -1,6 +1,12 @@
 #SPARQL_ENDPOINT=http://data.bibliotheken.nl/sparql  SPARQL_GRAPH="http://data.bibliotheken.nl/thesp/"  FILE_NAME_PREFIX="kb_partition=thesp_set=thes" mvn exec:java -Dexec.mainClass="com.tenforce.etms.Main"
 #SPARQL_ENDPOINT=http://data.bibliotheken.nl/sparql  SPARQL_GRAPH="http://data.bibliotheken.nl/thesc/"  FILE_NAME_PREFIX="kb_partition=thesc_set=thes" mvn exec:java -Dexec.mainClass="com.tenforce.etms.Main"
 #SPARQL_ENDPOINT=http://data.bibliotheken.nl/sparql  SPARQL_GRAPH="http://data.bibliotheken.nl/thes/"  FILE_NAME_PREFIX="kb_partition=thes_set=thes" mvn exec:java -Dexec.mainClass="com.tenforce.etms.Main"
-for f in dumps/kb*thes.nt; do
+
+# kb_partition=person_set=thes_content=links.nt.bz2
+SPARQL_ENDPOINT=http://data.bibliotheken.nl/sparql  SPARQL_GRAPH="http://data.bibliotheken.nl/thesp/wikidata-alignment/"  FILE_NAME_PREFIX="kb_partition=person_set=thes_content=links_wd" mvn exec:java -Dexec.mainClass="com.tenforce.etms.Main"
+SPARQL_ENDPOINT=http://data.bibliotheken.nl/sparql  SPARQL_GRAPH="http://data.bibliotheken.nl/thesp/dbpedia/"  FILE_NAME_PREFIX="kb_partition=person_set=thes_content=links_dbpedia" mvn exec:java -Dexec.mainClass="com.tenforce.etms.Main"
+SPARQL_ENDPOINT=http://data.bibliotheken.nl/sparql  SPARQL_GRAPH="http://data.bibliotheken.nl/nta-dbnla-alignment/"  FILE_NAME_PREFIX="kb_partition=person_set=thes_content=links_ntba" mvn exec:java -Dexec.mainClass="com.tenforce.etms.Main"
+
+for f in dumps/kb*.nt; do
 	lbzip2 $f
 done
